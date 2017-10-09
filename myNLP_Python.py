73f2242533a89dd7e7e83faae1bac89e362824f7
NikhilKorati <nikhil.koratiprasanna@ucdenver.edu>
2017-10-08 18:35:05 -0600

    Add files via upload

---
+++ myNLP_Python.py
@@ -0,0 +1,73 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
+
+#Cleaning the texts
+#import re
+#review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][0], )
+#review = review.lower()
+#
+##remove irrelavant words
+#import nltk
+#nltk.download('stopwords')
+#from nltk.corpus import stopwords
+#
+#review = review.split()
+#review = [word for word in review if not word in set(stopwords.words('english'))]
+#
+#
+##stemming
+#from nltk.stem.porter import PorterStemmer
+#ps = PorterStemmer()
+#review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+#
+#
+##convert list back to string
+#review =  str(' '.join(review))
+
+
+#Repeat the above steps for all reviews
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+corpus = []
+for i in range(0,len(dataset)):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i], )
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review =  str(' '.join(review))
+    corpus.append(review)
+
+
+#Create bag of words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:,1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+# Fitting classifier to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
